aea6a609436a12261baacdacc99eda649c9453b9
EvangeliaChatz <evangelia_hatz@hotmail.com>
2023-06-30 18:16:19 +0300

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: npm run startproject -- --port $PORT
--- model/model_pg.mjs
+++ model/model_pg.mjs
@@ -5,13 +5,20 @@ const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   ssl: false,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 async function connect() {
